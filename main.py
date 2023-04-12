e7f6cfe1491b1851ae856483efdeff7be33d565d
EDWIN <2105320@brunel.ac.uk>
2023-04-12 02:29:49 +0100

    V1

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 import pygame
-import sys
-from sys import exit
+import random
 
 screen_width = 1200
 screen_height = 700
+FPS = 60
 
 pygame.init()
+# mixer is for the sounds of the game
+pygame.mixer.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Platformer')
 clock = pygame.time.Clock()
 
-
-while True:
+run = True
+while run:
+    clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+            run = False
 
-    screen.fill('black')
+    # updates
 
+    # draw
+    screen.fill('grey')
+    # flipping the display?
+    pygame.display.flip()
 
-    pygame.display.update()
-    clock.tick(60)
-
-
-
+pygame.quit()
