2b382603fdef5e1e1ff47cae9e0449f59bed3b6f
EDWIN <2105320@brunel.ac.uk>
2023-04-14 17:36:33 +0100

    V2

--- main.py
+++ main.py
@@ -1,29 +1,74 @@
 import pygame
+from pygame.locals import *
 import random
+
 
 screen_width = 1200
 screen_height = 700
 FPS = 60
 
 pygame.init()
-# mixer is for the sounds of the game
-pygame.mixer.init()
+
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Platformer')
 clock = pygame.time.Clock()
 
+bg_img = pygame.image.load('graphics/bg1.jpg')
+
+tile_size = 32
+
+
+def draw_grid():
+    for line in range(0,40):
+        pygame.draw.line(screen, (255,255,255), (0, line * tile_size), (screen_width, line * tile_size))
+        pygame.draw.line(screen, (255,255,255), (line * tile_size, 0), (line * tile_size, screen_height))
+
+
+class World():
+    def __int__(self, data):
+        self.tile_list = []
+
+        dirt_img = pygame.image.load('graphics/Tile.png')
+
+        row_count = 0
+        for row in data:
+            col_count = 0
+            for tile in row:
+                if tile == 1:
+                    img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                col_count += 1
+            row_count += 1
+
+
+world_data = [
+    [1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1],
+    ]
+
+world = World(world_data)
+
 run = True
 while run:
     clock.tick(FPS)
+
+    screen.blit(bg_img, (0, 0))
+
+    draw_grid()
+    print(world.tile_list)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-    # updates
-
-    # draw
-    screen.fill('grey')
     # flipping the display?
-    pygame.display.flip()
+    pygame.display.update()
 
 pygame.quit()
---
+++ settings.py
@@ -0,0 +1,13 @@
+import pygame
+import random
+
+
+class World():
+    def __init__(self, data):
+        # image loading
+        dirt_img = pygame.image.load()
+
+        #for row in data:
+            #for tile in row
+                #print()
+
