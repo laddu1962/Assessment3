7d8adab67ce6b51f9a4a526cd2ca4f647a6124a0
EDWIN <2105320@brunel.ac.uk>
2023-04-15 00:16:16 +0100

    world - class

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 import pygame
 from pygame.locals import *
-import random
-
-
-screen_width = 1200
-screen_height = 700
-FPS = 60
 
 pygame.init()
 
+screen_width = 1200
+screen_height = 700
+
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Platformer')
-clock = pygame.time.Clock()
+
+
+tile_size = 64
 
 bg_img = pygame.image.load('graphics/bg1.jpg')
-
-tile_size = 32
 
 
 def draw_grid():
@@ -25,10 +22,12 @@ def draw_grid():
 
 
 class World:
-    def __int__(self, data):
+    def __init__(self, data):
         self.tile_list = []
 
         dirt_img = pygame.image.load('graphics/Tile.png')
+        left_img = pygame.image.load('graphics/Tile_01.png')
+        right_img = pygame.image.load('graphics/Tile_03.png')
 
         row_count = 0
         for row in data:
@@ -36,6 +35,20 @@ class World:
             for tile in row:
                 if tile == 1:
                     img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 2:
+                    img = pygame.transform.scale(left_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 3:
+                    img = pygame.transform.scale(right_img, (tile_size, tile_size))
                     img_rect = img.get_rect()
                     img_rect.x = col_count * tile_size
                     img_rect.y = row_count * tile_size
@@ -50,26 +63,29 @@ class World:
 
 
 world_data = [
-    [1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1],
-    ]
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 2, 1, 1, 3, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
 
 world = World(world_data)
 
 run = True
 while run:
-    clock.tick(FPS)
 
     screen.blit(bg_img, (0, 0))
 
     world.draw()
 
     draw_grid()
-
-    print(world.tile_list)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
